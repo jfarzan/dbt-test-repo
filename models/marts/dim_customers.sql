77e58d8c54e27d61849372089f99766b195c6d77
Javed Farzan <javedfarzan@gmail.com>
2026-09-16 23:57:46 +0000

    organized folders, added stage tables

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,17 +1,8 @@
 with customers as (
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    from raw.jshop.customers
+  select * from {{ ref('stg_jshop__customers') }}
 ),
 orders as (
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from raw.jshop.orders
+  select * from {{ ref('stg_jshop__orders') }}
 ),
 customer_orders as (
     select
